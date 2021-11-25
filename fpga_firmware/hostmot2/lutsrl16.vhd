8576e919e8547fd02c9d91078fd7c10b9b83c700
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2021-11-25 14:11:31 +0200

    Import Mesa Hostmot2 firmware

---
+++ fpga_firmware/hostmot2/lutsrl16.vhd
@@ -0,0 +1,111 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+--
+-- Copyright (C) 2007, Peter C. Wallace, Mesa Electronics
+-- http://www.mesanet.com
+--
+-- This program is is licensed under a disjunctive dual license giving you
+-- the choice of one of the two following sets of free software/open source
+-- licensing terms:
+--
+--    * GNU General Public License (GPL), version 2.0 or later
+--    * 3-clause BSD License
+-- 
+--
+-- The GNU GPL License:
+-- 
+--     This program is free software; you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation; either version 2 of the License, or
+--     (at your option) any later version.
+-- 
+--     This program is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+-- 
+--     You should have received a copy of the GNU General Public License
+--     along with this program; if not, write to the Free Software
+--     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+-- 
+-- 
+-- The 3-clause BSD License:
+-- 
+--     Redistribution and use in source and binary forms, with or without
+--     modification, are permitted provided that the following conditions
+--     are met:
+-- 
+--         * Redistributions of source code must retain the above copyright
+--           notice, this list of conditions and the following disclaimer.
+-- 
+--         * Redistributions in binary form must reproduce the above
+--           copyright notice, this list of conditions and the following
+--           disclaimer in the documentation and/or other materials
+--           provided with the distribution.
+-- 
+--         * Neither the name of Mesa Electronics nor the names of its
+--           contributors may be used to endorse or promote products
+--           derived from this software without specific prior written
+--           permission.
+-- 
+-- 
+-- Disclaimer:
+-- 
+--     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+--     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+--     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+--     FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+--     COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+--     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+--     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+--     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+--     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+--     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+--     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+--     POSSIBILITY OF SUCH DAMAGE.
+-- 
+
+entity lutsrl16 is		-- lut based srl16
+    generic ( 
+		init : std_logic_vector(15 downto 0) := X"0000"
+		);
+    port ( 
+          D   : in  std_logic;
+          CE  : in  std_logic;
+          CLK : in  std_logic;
+          A0  : in  std_logic;
+          A1  : in  std_logic;
+          A2  : in  std_logic;
+          A3  : in  std_logic;
+          Q   : out std_logic 
+		);
+end lutsrl16;
+
+architecture Behavioral of lutsrl16 is
+signal sreg : std_logic_vector(15 downto 0) := init;
+signal addr : std_logic_vector(3 downto 0);
+
+begin
+
+	a_lutsrl16_reg: process (CLK,D,A0,A1,A2,A3,CE)
+
+	begin
+		if clk'event and clk = '1' then
+			if CE = '1' then
+				 sreg(15 downto 1) <= sreg(14 downto 0);
+				 sreg(0) <= D;
+			end if;
+		end if; -- clk
+		addr(0) <= A0;
+		addr(1) <= A1;
+		addr(2) <= A2;
+		addr(3) <= A3;
+		for i in 0 to 15 loop
+			if i = addr then
+				Q <= sreg(i);
+			end if;
+		end loop;	
+	end process;
+end Behavioral;
