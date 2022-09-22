b11adbfcc863a87e07bb3ad3f81594ab3fc2dbd2
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2022-09-22 21:51:36 +0300

    Improve coordinate system handling, add selector buttons to sidebar

---
+++ linuxcnc/config/gladevcp_sidebar.py
@@ -0,0 +1,36 @@
+import linuxcnc
+from hal_glib import GStat
+import time
+GSTAT = GStat()
+
+class SidebarHandler:
+    def __init__(self, halcomp, builder, useropts):
+        self.builder = builder
+        self.command = linuxcnc.command()
+
+        GSTAT.connect("user-system-changed", self.update_coordinate_selection)
+        GSTAT.connect("all-homed", self.activate_default_g54)
+
+    def activate_default_g54(self, w):
+        time.sleep(0.1)
+        self.command.mode(linuxcnc.MODE_MDI)
+        self.command.mdi("G55")
+        self.command.wait_complete()
+        self.command.mdi("G54")
+        self.command.wait_complete()
+
+    def update_coordinate_selection(self, w, data):
+        system = int(data)
+        self.builder.get_object('selectg54').set_active(system == 1)
+        self.builder.get_object('selectg55').set_active(system == 2)
+        self.builder.get_object('selectg56').set_active(system == 3)
+        self.builder.get_object('selectg57').set_active(system == 4)
+
+        other_sys = (system not in [1, 2, 3, 4])
+        self.builder.get_object('selectg54').set_inconsistent(other_sys)
+        self.builder.get_object('selectg55').set_inconsistent(other_sys)
+        self.builder.get_object('selectg56').set_inconsistent(other_sys)
+        self.builder.get_object('selectg57').set_inconsistent(other_sys)
+
+def get_handlers(halcomp,builder,useropts):
+    return [SidebarHandler(halcomp,builder,useropts)]
