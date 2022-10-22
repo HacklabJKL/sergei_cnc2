9dabb46afe1492b652f371196059a260c04849a6
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2022-10-22 10:42:39 +0300

    Add workaround for LinuxCNC/linuxcnc#177 (#19)
    LinuxCNC 2.8 has a bug where emergency stop resets the G64 path
    blending tolerance. This causes excessive rounding of corners.
    It is fixed in 2.9 but I want to keep with stable version for now.
    
    This works around the issue by reissuing G64 P0.01 Q0.01 after
    state-on and all-homed events.

--- linuxcnc/config/gladevcp_sidebar.py
+++ linuxcnc/config/gladevcp_sidebar.py
@@ -10,8 +10,11 @@ class SidebarHandler:
 
         GSTAT.connect("user-system-changed", self.update_coordinate_selection)
         GSTAT.connect("all-homed", self.activate_default_g54)
+        GSTAT.connect("all-homed", self.verify_g64)
+        GSTAT.connect("state-on", self.verify_g64)
 
     def activate_default_g54(self, w):
+        '''Make sure G54 coordinate system is active after start.'''
         time.sleep(0.1)
         self.command.mode(linuxcnc.MODE_MDI)
         self.command.mdi("G55")
@@ -20,6 +23,7 @@ class SidebarHandler:
         self.command.wait_complete()
 
     def update_coordinate_selection(self, w, data):
+        '''Update the state of coordinate selection buttons in sidebar'''
         system = int(data)
         self.builder.get_object('selectg54').set_active(system == 1)
         self.builder.get_object('selectg55').set_active(system == 2)
@@ -31,6 +35,21 @@ class SidebarHandler:
         self.builder.get_object('selectg55').set_inconsistent(other_sys)
         self.builder.get_object('selectg56').set_inconsistent(other_sys)
         self.builder.get_object('selectg57').set_inconsistent(other_sys)
+
+    def verify_g64(self, w, data = None):
+        '''If G64 (interpolation) is active, verify sane tolerances.
+        This is to workaround LinuxCNC issue: https://github.com/LinuxCNC/linuxcnc/issues/177
+        '''
+
+        time.sleep(0.05)
+        s = linuxcnc.stat()
+        s.poll()
+
+        all_homed = (0 not in s.homed[:3])
+        g64_active = (640 in s.gcodes)
+        if all_homed and g64_active:
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi("G64 P0.01 Q0.01")
 
 def get_handlers(halcomp,builder,useropts):
     return [SidebarHandler(halcomp,builder,useropts)]
